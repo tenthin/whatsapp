d4a22fe9c7da6bb2d15196a0d391714258682e14
Tenzin <tenthinten@gmail.com>
2022-03-21 22:17:23 +0530

    app and add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Chat from "./Components/Chat";
+import Sidebar from "./Components/Sidebar";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import { useStateValue } from "./Components/StateProvider";
 
 function App() {
-  return (
-    <div className="App">
+  const [{ user }, dispatch] = useStateValue();
 
+  return (
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/" />
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
